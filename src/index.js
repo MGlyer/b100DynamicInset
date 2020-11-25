d5cd9526aae06a0ae17b33cf3f653c143f2941a4
Martin Glyer <martin.glyer@dowjones.com>
2020-11-25 11:46:33 -0800

    puts base on page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,75 @@
 import { slug } from '../inset/data.json';
 import './index.scss';
+import dataMap from '../utils/dataMap';
+
 
 (() => {
   const element = document.getElementById(`${slug}-container`);
+  dataMap.forEach((item) => {
+    const target = document.getElementById(item.id);
+    const targetLabel = document.createElement("label");
+    const targetField = document.createElement("input");
 
-  element.innerHTML = 'Inset contents go here. Maybe a chart, maybe something else!';
-  element.style.background = 'lightgray';
-  element.style.height = '250px';
+    targetLabel.innerHTML = item.text;
+    targetLabel.className = 'input-label'
+    target.appendChild(targetLabel);
+    target.appendChild(targetField);
+  })
+
+  // element.innerHTML = 'Inset contents go here. Maybe a chart, maybe something else!';
+  // element.style.background = 'lightgray';
+  element.style.height = 'fit-content';
+
+  const validateEmail = (email) => {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+  }
+
+  const checkValidity = (field, inputVal) => {
+    if (field.required && inputVal === '') {
+      return false;
+    }
+    if (field.description.toLowerCase().includes('email') && !this.validateEmail(inputVal)) {
+      return false;
+    }
+    return true;
+  }
+  
+  const sendFeedback = (evt) => {
+    const rating = [...evt.target.classList].includes('positive') ? 1 : 0;
+    const user_type = window.utag ? utag.data.user_type : utag_data.user_type;
+    const payload = {
+      "email_4E75E544-ABF9-4FDC-8A15-BE4806ADC894": "Nominator's email RESPONSE VAL",
+      "name_98F5D95D-8FBE-4A79-9A9F-9A0702E07078": "Nominator's name RESPONSE VAL",
+      "text_input_789CFD48-7D0A-405B-846B-2CA9795706A4": "Nominator's company, job title RESPONSE VAL",
+      "contact_1933B26A-1BB2-45AF-88DB-AFAA27C5FB36": "Nominator's contact info (phone) RESPONSE VAL",
+      "text_input_63176022-E1BA-406E-B334-DAA0BF8DE133": "Nominator's relationship to nominee RESPONSE VAL",
+      "dropdown_8982D47C-423E-4CCE-A6D5-95F841E29E2F": "Do you want to your identity to be revealed as a nominator? RESPONSE VAL",
+      "text_input_136F01AF-6B86-45C9-866B-2395BECA51A5": "Nominee's name RESPONSE VAL",
+      "text_input_E68A6D22-CB04-4820-9051-46DA2F00C525": "Nominee's company and job title RESPONSE VAL",
+      "email_B093DF61-FA5A-4EB5-B1DF-9881BDD81108": "Nominee's email RESPONSE VAL",
+      "contact_93B4DCAA-86DA-4C0C-B144-70423C3DA0F2": "Nominee's contact (phone) RESPONSE VAL",
+      "text_area_564A8B4C-2818-4D2B-9E0B-ECA2CEED5EA1": "Nominee's education RESPONSE VAL",
+      "text_area_861A436F-D4B6-41F4-8A3E-300288C661C3": "Description of nominee's current job or role, including key responsibilities RESPONSE VAL",
+      "text_area_2D42ECB1-0B55-4CC4-9F6B-F964BBE3BC45": "Nominee's achievements and successes in this job RESPONSE VAL",
+      "text_area_92E98DF2-F9B2-40BF-B257-F241990982D3": "Describe the nominee's influence within the firm and industry RESPONSE VAL",
+      "text_area_AC170692-38BD-4B86-9E00-DBFAF22FECD7": "Nominee's role as a mentor in the workplace RESPONSE VAL",
+      "text_area_A2118387-7F86-4FBB-821D-6953F337C1C1": "Nominee's prior jobs or positions RESPONSE VAL",
+      "text_area_7130C671-C18A-4CA5-A833-78DE06C9747D": "Testimonials, if any, from mentors, bosses, clients or peers RESPONSE VAL"
+    }
+    const formUrl = 'https://10nk3u5ov4.execute-api.us-east-1.amazonaws.com/prod/v1/feedback/cd48f026-346a-4ee6-b934-e273620ae07d';
+
+    fetch(formUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload),
+    }).then(response => response.json())
+    .then(resJson => {
+      if(resJson.id) {
+        formElem.innerHTML = '<p style="align-self: center;">Thank you for your feedback!</p>';
+      }
+    })
+  };
 })();
---
+++ utils/dataMap.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    id: 'nom-name',
+    text: 'Name of Nominee*',
+    slug: 'text_input_136F01AF-6B86-45C9-866B-2395BECA51A5'
+  },
+  {
+    id: 'nom-company-job-title',
+    text: 'Nominee\'s company and job title*',
+    slug: 'text_input_E68A6D22-CB04-4820-9051-46DA2F00C525'
+  },
+  {
+    id: 'nom-email',
+    text: 'Nominee\'s Email*',
+    slug: 'email_B093DF61-FA5A-4EB5-B1DF-9881BDD81108'
+  },
+  {
+    id: 'nom-phone',
+    text: `Nominee's Phone*`,
+    slug: 'contact_93B4DCAA-86DA-4C0C-B144-70423C3DA0F2'
+  },
+  {
+    id: 'nom-education',
+    text: `Nominee's Education`,
+    slug: 'text_area_564A8B4C-2818-4D2B-9E0B-ECA2CEED5EA1'
+  },
+  {
+    id: 'nom-description',
+    text: `Brief Description of nominee's current job or role, including key responsibilities`,
+    slug: 'text_area_861A436F-D4B6-41F4-8A3E-300288C661C3'
+  },
+  {
+    id: 'nom-achievements',
+    text: `Nominee's achievements and successes in this job (can include promotions, important deals, business wins, investment performance, revenue generation, research published, products created, and such)*`,
+    slug: 'text_area_2D42ECB1-0B55-4CC4-9F6B-F964BBE3BC45'
+  },
+  {
+    id: 'nom-influence',
+    text: `Describe the nominee's influence within the firm and industry *`,
+    slug: 'text_area_92E98DF2-F9B2-40BF-B257-F241990982D3'
+  },
+  {
+    id: 'nom-mentor',
+    text: `Nominee's role as a mentor in the workplace (Please provide examples.) *`,
+    slug: 'text_area_AC170692-38BD-4B86-9E00-DBFAF22FECD7'
+  },
+  {
+    id: 'nom-prior',
+    text: `Nominee's prior jobs or positions`,
+    slug: 'text_area_A2118387-7F86-4FBB-821D-6953F337C1C1'
+  },
+  {
+    id: 'nom-testimonials',
+    text: `Testimonials, if any, from mentors, bosses, clients, or peers (provide links, or contact information for people who would like to support this nomination)`,
+    slug: 'text_area_7130C671-C18A-4CA5-A833-78DE06C9747D'
+  },
+  {
+    id: 'nom-activities',
+    text: `Nominee's outside activities (organizational leadership, board membership, nonprofit work, other interests or activities) *`,
+    slug: ''
+  },
+  {
+    id: 'submitter-email',
+    text: `Nominator's Email`,
+    slug: 'email_4E75E544-ABF9-4FDC-8A15-BE4806ADC894'
+  },
+  {
+    id: 'submitter-phone',
+    text: `Nominator's Phone`,
+    slug: 'contact_1933B26A-1BB2-45AF-88DB-AFAA27C5FB36'
+  },
+  {
+    id: 'submitter-details',
+    text: `Nominator’s Name, Company, Job Title *`,
+    slug: 'name_98F5D95D-8FBE-4A79-9A9F-9A0702E07078'
+  },
+  {
+    id: 'submitter-relationship',
+    text: `Nominator’s Relationship  to Nominee *`,
+    slug: 'text_input_63176022-E1BA-406E-B334-DAA0BF8DE133'
+  }
+]
